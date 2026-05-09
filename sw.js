1584e2e42a078c9a37e695f2a918a298287db1ac
ivanbj96 <64235647+ivanbj96@users.noreply.github.com>
2026-05-09 11:47:53 -0500

    v7.20.40 - fix backup duplicado: cliente no envia si usuario autenticado en nube

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker con versionado automatico
-const APP_VERSION = '7.20.39'; // fix iconos APK: foreground tamanos correctos, ic_notification blanco+alpha
+const APP_VERSION = '7.20.40'; // fix backup duplicado: cliente no envia si usuario autenticado en nube
 const CACHE_NAME = `galloli-v${APP_VERSION}`;
 const DATA_CACHE_NAME = `galloli-data-v${APP_VERSION}`;
 
